cf7821da560dc1ba2453f01e18e7cdcf0e61f6ba
Artur <zentabl@yandex.ru>
2020-06-21 22:17:01 +0400

    removed the error and added overwriting of the used file "spisok"

--- filtration.py
+++ filtration.py
@@ -3,13 +3,21 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
 import re
 import time
+
+#Убираем ошибку  ERROR:gl_surface_egl.cc(750)] EGL Driver message (Error) eglQueryDeviceAttribEXT: Bad attribute.
+
+chrome_opt = webdriver.ChromeOptions()
+chrome_opt.add_argument('--disable-gpu')
+path = r"E:\\Gittext\\chromedriver.exe"
+browser = webdriver.Chrome(executable_path=path,chrome_options=chrome_opt)
+
 # Параметры фильтрации
 
 days = 20 # Дней с момента последней публикации
 acc_subscriptions = 500 # Количество подписок у аккаунта
 publications = 10 # Количество публикаций у аккаунта
 
-browser = webdriver.Chrome("E:\Gittext\chromedriver.exe")
+#browser = webdriver.Chrome("E:\Gittext\chromedriver.exe")
 
 # Проверка существования элемента на странице
 
@@ -41,29 +49,30 @@ for person in file_list:
     j += 1
     browser.get(person)
     time.sleep(0.6)
-
+    
     # Проверка закрытости аккаунта
 
     element = '//section/main/div/div/article/div/div/h2'
     if xpath_existence(element) == 1:
-        try:
-            if browser.find_element_by_xpath(element).text == "This Account is Private" or "Это закрытый аккаунт":
-                print(j, "Приватный аккаунт")
-                continue
-        except StaleEleentReferenceException:
-            print ("Ошибка, код ошибки - 1")
+        if browser.find_element_by_xpath(element).text == "This Account is Private" or "Это закрытый аккаунт":
+            print(j, "Приватный аккаунт")
+            file_list.remove(person)    
+                
+            continue
 
     # Проверка на количество подписок
 
     element = "//section/main/div/header/section/ul/li[3]/a/span"
     if xpath_existence(element) == 0:
         print (j, "Код ошибки - 2")
+        file_list.remove(person)
         continue
     status = browser.find_element_by_xpath(element).text
     status = re.sub(r'\s', '', status) # удаление пробелов из числа подписок (в коде xpath)
 
     if int(status) > acc_subscriptions:
         print (j, "У аккаунта больше ", acc_subscriptions, "подписок")
+        file_list.remove(person)
         continue
 
     # Проверка ссылки на сайт (коммерческий ли аккаунт)
@@ -71,6 +80,7 @@ for person in file_list:
     element = "//section/main/div/header/section/div[2]/a"
     if xpath_existence(element) == 1:
         print(j, "Есть ссылка на сайт, пропускаем")
+        file_list.remove(person)
         continue
 
     # Проверка на наличие публикаций в аккаунте
@@ -78,22 +88,34 @@ for person in file_list:
     element = "//section/main/div/header/section/ul/li[1]/a/span"
     if xpath_existence(element) == 0:
         print(j, "Ошибка")
+        
+        file_list.remove(person)
         continue
     status = browser.find_element_by_xpath(element).text #повторение кода!
     status = re.sub(r'\s', '', status) # удаление пробелов из числа подписок (в коде xpath)
     if int(status) < publications:
         print (j, "У аккаунта меньше ", publications, "публикаций")
+        file_list.remove(person)
         continue
 
     # Добавление пользователя в отфильтрованный файл
 
     filtered_list.append(person)
+    
     print(j, "Добавлен новый пользователь", person)
     i += 1
-    if i > 1:
+    
+    if i >= 10:
         break
 
-    
+# Перезапись файла со списком пользователей, что бы в следующее обращение к нему не отлфильровывать этот список повторно
+
+f = open("E:\Gittext\spisok.txt", 'w')
+for newperson in file_list:
+    f.write(newperson)
+        
+f.close()
+        
 
 
 f = open("E:\\Gittext\\filtered_spisok.txt", 'w')
